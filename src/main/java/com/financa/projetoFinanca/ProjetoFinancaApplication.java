34e9b1a5d8dd9fe8267610b8ce0bc7990a6ec99e
RobsonFranco <robson14@hotmail.com.br>
2024-03-10 15:51:51 -0300

    Inicializaer

---
+++ src/main/java/com/financa/projetoFinanca/ProjetoFinancaApplication.java
@@ -0,0 +1,13 @@
+package com.financa.projetoFinanca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFinancaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFinancaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/financa/projetoFinanca/ProjetoFinancaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financa.projetoFinanca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFinancaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
